7e2fbc2e45f1b444da08b6540fd27a8e99dd49a2
Chí Thành <chithanh171004@gmail.com>
2024-05-17 20:06:06 +0700

    dá

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const app = express();
 const cors = require("cors");
 
-app.use(cors({ origin: "https://yi-di-electro-api.vercel.app/" })); // Use this to allow all origins
+app.use(cors({ origin: "https://yi-di-electro-api.vercel.app" })); // Use this to allow all origins
 
 const dataCars = JSON.parse(fs.readFileSync("./data/dataCar.json", "utf-8"));
 const dataUsers = JSON.parse(fs.readFileSync("./data/dataUser.json", "utf-8"));
