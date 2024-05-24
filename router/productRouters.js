ecfe4958743094c529ee78a4061895068d79c799
Chí Thành <chithanh171004@gmail.com>
2024-05-24 12:07:16 +0700

    fix server

--- router/productRouters.js
+++ router/productRouters.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
-const allProduct = JSON.parse(fs.readFileSync("./data/dataCar.json", "utf-8"));
 
 router.route("/").get((req, res) => {
+  const allProduct = JSON.parse(
+    fs.readFileSync("./data/dataCar.json", "utf-8")
+  );
   const product = allProduct
     .map(
       (product) =>
