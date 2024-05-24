c9f229c9be8b605479299a3325112fd365b47195
Chí Thành <chithanh171004@gmail.com>
2024-05-24 14:43:18 +0700

    fix redirect

--- router/homeRouters.js
+++ router/homeRouters.js
@@ -5,9 +5,9 @@ const tokenManager = require("../util/tokenManager");
 router.route("/").get((req, res) => {
   if (req.query.token == "0123456789") {
     tokenManager.setToken(req.query.token);
-    return res.send("Bạn cần có quyền truy cập trang");
+    return res.redirect("/dashboard");
   } else {
-    return res.redirect("https://yidi-electro.vercel.app/admin");
+    return res.send("Bạn cần có quyền truy cập trang");
   }
 });
 
